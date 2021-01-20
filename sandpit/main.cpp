a6d492959c7bfd7fb03adabfa0e829218d58dd8f
savaged <savagedavid@gmail.com>
2021-01-20 10:51:00 +0000

    tinkering with c++ to understand interfaces

--- sandpit/main.cpp
+++ sandpit/main.cpp
@@ -2,14 +2,52 @@
 
 using namespace std;
 
+class Interface
+{
+public:
+    virtual int getA() = 0;
+};
+
+class Base : public Interface
+{
+    int _a;
+public:
+    Base() : _a(42) { }
+    Base(int a) : _a(a) { }
+
+    int getA() override 
+    {
+        return _a;
+    }
+};
+
+class Derived : public Base
+{
+    int _b;
+public:
+    Derived(int a, int b) : Base(a), _b(b) { }
+
+    int getB()
+    {
+        return _b;
+    }
+};
+
+void PrintA(Interface* object)
+{
+    cout << object->getA() << endl;
+}
+
 int main()
 {
-    int a = 42;
-    int *p = &a;
-    int b = *p;
+    Base* b = new Base();
+    Derived* d = new Derived(1, 2);
 
-    cout << p << endl;
-    cout << b << endl;
+    cout << b->getA() << endl;
+    cout << d->getA() << " " << d->getB() << endl;
+    
+    PrintA(b);
+    PrintA(d);
     
     return 0;
 }
